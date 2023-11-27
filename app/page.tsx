159933564242b8922653dc446c3b34928b607578
Ankit Kokane <ankitkokane90@gmail.com>
2023-11-28 00:52:58 +0530

    base landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
 
     <main className=' min-h-screen'>
-      <div className='flex flex-row px-40 h-screen'>
-          <div className='flex flex-col'>
-
+      <div className='flex flex-row px-40 h-screen bg-[#79a2d8]'>
+        <Image src='/chain.png' alt='' fill className='-z-0 opacity-60'/>
+        <div className='flex flex-col mt-auto mb-14 z-30'>
+          <div className='flex flex-col gap-4'>
+            <p className='text-5xl font-semibold'>Decentralized Powers</p>
+            <p className='text-5xl font-semibold'>Effortless Update</p>
+            <p className='text-xl flex flex-col'><span>Revolutionize your software experience with decentralized updates.</span> <span>Trust, security, and seamless evolution — all in one platform.</span></p>
           </div>
-          <div className='ml-auto my-auto'>
-            <Image src='/blockchain_doodle_cricle-removebg-preview.png' alt='' width='100' height='100'/>
-          </div>
+          <Button className='w-fit mt-5'>New Project</Button>
+        </div>
+        <div className='relative w-[70vh] ml-auto'>
+          {/* <Image src='/blockchain_doodle_cricle-removebg-preview.png' alt='' fill
+            quality={100}
+            style={{
+              objectFit: 'contain',
+            }}
+            className='' /> */}
+        </div>
       </div>
     </main>
   )
