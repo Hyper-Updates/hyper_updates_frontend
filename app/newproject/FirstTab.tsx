6dc074b5dc357b1a5d1cab2b3934c7785061ce4c
Ankit Kokane <ankitkokane90@gmail.com>
2023-12-05 17:33:17 +0530

    solving build error

---
+++ app/newproject/FirstTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FirstTab() {
+  return (
+    <div>
+        <div>
+            Create A New Project Repository
+        </div>
+    </div>
+  )
+}
--- app/newproject/page.tsx
+++ app/newproject/page.tsx
@@ -1,31 +1,33 @@
 "use client"
 import { StepProgress } from '@/components/StepProgress'
 import React, { useState } from 'react'
+import FirstTab from './FirstTab'
 
-export default function page() {
+export default function NewPoject() {
     const [currentStep, setCurrentStep] = useState<number>(2)
     const steps = [
         {
-          label: 'Address',
-          step: 1,
+            label: 'Address',
+            step: 1,
         },
         {
-          label: 'Shipping',
-          step: 2,
+            label: 'Shipping',
+            step: 2,
         },
         {
-          label: 'Payment',
-          step: 3,
+            label: 'Payment',
+            step: 3,
         },
-        {
-          label: 'Summary',
-          step: 4,
-        },
-      ]
+    ]
     return (
-        <div className='px-40'>
-            <div className='my-40 px-40 '>
-                <StepProgress currentStep={currentStep} steps={steps} />
+        <div className='px-40 py-40'>
+            <div className='border shadow rounded flex flex-col'>
+                <div className='py-10 border-b mx-40'>
+                    <StepProgress currentStep={currentStep} steps={steps} />
+                </div>
+                <div className='pt-10 mx-40'>
+                    <FirstTab/>
+                </div>
             </div>
         </div>
     )
